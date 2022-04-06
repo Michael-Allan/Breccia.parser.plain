515e03727f7924ef06a8a009f1873b2ff45c3f62
Michael Allan <mike@reluk.ca>
2022-04-05 23:19:19 -0400

    Buffering for sake of speed.

--- Project.java
+++ Project.java
@@ -3,9 +3,6 @@ package Breccia.parser.plain;
 import java.io.*;
 import java.nio.file.Path;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.nio.file.Files.newInputStream;
-
 
 /** The present project.  Included are one or two miscellaneous resources,
   * residual odds and ends that properly fit nowhere else.
@@ -22,13 +19,8 @@ public final class Project {
     public static Reader newSourceReader( final Path sourceFile ) throws IOException { /* Little point
            in dealing with the `IOException` at this level, because anyway the caller must deal with it
            on closing the reader, usually by appending a `catch` to a try-with-resources block. */
-        final InputStream byteSource = newInputStream​( sourceFile );
-        final InputStreamReader charSource = new InputStreamReader( byteSource, UTF_8 );
-          // Any reasonable implementation of `InputStreamReader` is going to maintain an adequate
-          // byte buffer for bulk transfer requests.  Further buffering of `byteSource` would likely
-          // be counter-productive.  https://stackoverflow.com/a/27347262/2402790
-        return charSource; }}
+        return java.nio.file.Files.newBufferedReader​( sourceFile ); }}
 
 
 
-                                                        // Copyright © 2021  Michael Allan.  Licence MIT.
+                                                   // Copyright © 2021-2022  Michael Allan.  Licence MIT.

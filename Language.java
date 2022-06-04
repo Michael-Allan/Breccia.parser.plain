e29d361cc24a974fd8df9a5d6899d23337856191
Michael Allan <mike@reluk.ca>
2022-06-04 07:57:16 -0400

    Publication.

--- Language.java
+++ Language.java
@@ -1,7 +1,7 @@
 package Breccia.parser.plain;
 
 
-final class Language {
+public final class Language {
 
 
     private Language() {}
@@ -11,50 +11,50 @@ final class Language {
     /** Whether character  `ch` is one that formally ends a newline.
       * Returns true if `ch` is a line feed (A).
       */
-    static boolean completesNewline( final char ch ) { return ch == '\n'; }
+    public static boolean completesNewline( final char ch ) { return ch == '\n'; }
 
 
     /** Whether code point `ch` is one that formally ends a newline.
       * Returns true if `ch` is a line feed (A).
       */
-    static boolean completesNewline( final  int ch ) { return ch == '\n'; }
+    public static boolean completesNewline( final  int ch ) { return ch == '\n'; }
 
 
 
     /** Whether character  `ch` is proper to a newline.
       * Returns true if `ch` is a line feed (A) or carriage return (D).
       */
-    static boolean impliesNewline( final char ch ) { return ch == '\n' || ch == '\r'; }
+    public static boolean impliesNewline( final char ch ) { return ch == '\n' || ch == '\r'; }
 
 
     /** Whether code point `ch` is proper to a newline.
       * Returns true if `ch` is a line feed (A) or carriage return (D).
       */
-    static boolean impliesNewline( final  int ch ) { return ch == '\n' || ch == '\r'; }
+    public static boolean impliesNewline( final  int ch ) { return ch == '\n' || ch == '\r'; }
 
 
 
     /** Whether character  `ch` is proper to a newline, yet does not formally complete it.
       * Returns true if `ch` is a carriage return (D).
       */
-    static boolean impliesWithoutCompletingNewline( final char ch ) { return ch == '\r'; }
+    public static boolean impliesWithoutCompletingNewline( final char ch ) { return ch == '\r'; }
 
 
     /** Whether code point `ch` is proper to a newline, yet does not formally complete it.
       * Returns true if `ch` is a carriage return (D).
       */
-    static boolean impliesWithoutCompletingNewline( final  int ch ) { return ch == '\r'; }
+    public static boolean impliesWithoutCompletingNewline( final  int ch ) { return ch == '\r'; }
 
 
 
     /** Whether `ch` is a divider drawing character, a character in the range 2500-259F.
       */
-    static boolean isDividerDrawing( final char ch ) { return '\u2500' <= ch && ch <= '\u259F'; }
+    public static boolean isDividerDrawing( final char ch ) { return '\u2500' <= ch && ch <= '\u259F'; }
 
 
     /** Whether `ch` is a divider drawing character, a character in the range 2500-259F.
       */
-    static boolean isDividerDrawing( final  int ch ) { return '\u2500' <= ch && ch <= '\u259F'; }
+    public static boolean isDividerDrawing( final  int ch ) { return '\u2500' <= ch && ch <= '\u259F'; }
 
 
 
@@ -63,7 +63,7 @@ final class Language {
       *          &amp; !{@linkplain #impliesNewline(char) impliesNewline}
       *          &amp;  {@linkplain #yetIsGenerallyWhitespace(char) yetIsGenerallyWhitespace}`.
       */
-    static boolean isForbiddenWhitespace( final char ch ) {
+    public static boolean isForbiddenWhitespace( final char ch ) {
         return !isPlainWhitespace(ch) && yetIsGenerallyWhitespace(ch); }
 
 
@@ -72,43 +72,45 @@ final class Language {
       *          &amp; !{@linkplain #impliesNewline(int)  impliesNewline}
       *          &amp;  {@linkplain #yetIsGenerallyWhitespace(int)  yetIsGenerallyWhitespace}`.
       */
-    static boolean isForbiddenWhitespace( final  int ch ) {
+    public static boolean isForbiddenWhitespace( final  int ch ) {
         return !isPlainWhitespace(ch) && yetIsGenerallyWhitespace(ch); }
 
 
 
     /** Whether character  `ch` is a plain space (20) or newline constituent (A or D).
       */
-    static boolean isPlainWhitespace( final char ch ) { return ch == ' ' || impliesNewline(ch); }
+    public static boolean isPlainWhitespace( final char ch ) { return ch == ' ' || impliesNewline(ch); }
 
 
     /** Whether character  `ch` is a plain space (20) or newline constituent (A or D).
       */
-    static boolean isPlainWhitespace( final  int ch ) { return ch == ' ' || impliesNewline(ch); }
+    public static boolean isPlainWhitespace( final  int ch ) { return ch == ' ' || impliesNewline(ch); }
 
 
 
     /** Whether character  `ch` is a plain (20) or no-break space (A0).
       */
-    static boolean isSpace( final char ch ) { return ch == ' ' || ch == '\u00A0'; }
+    public static boolean isSpace( final char ch ) { return ch == ' ' || ch == '\u00A0'; }
 
 
     /** Whether code point `ch` is a plain (20) or no-break space (A0).
       */
-    static boolean isSpace( final  int ch ) { return ch == ' ' || ch == '\u00A0'; }
+    public static boolean isSpace( final  int ch ) { return ch == ' ' || ch == '\u00A0'; }
 
 
 
     /** Whether character `ch` is a plain space (20), no-break space (A0)
       * or newline constituent (A or D).
       */
-    static boolean isWhitespace( final char ch ) { return isPlainWhitespace(ch) || ch == '\u00A0'; }
+    public static boolean isWhitespace( final char ch ) {
+        return isPlainWhitespace(ch) || ch == '\u00A0'; }
 
 
     /** Whether character `ch` is a plain space (20), no-break space (A0)
       * or newline constituent (A or D).
       */
-    static boolean isWhitespace( final  int ch ) { return isPlainWhitespace(ch) || ch == '\u00A0'; }
+    public static boolean isWhitespace( final  int ch ) {
+        return isPlainWhitespace(ch) || ch == '\u00A0'; }
 
 
 
@@ -121,7 +123,7 @@ final class Language {
       *       or newline constituent.
       *     @see Java.Characters.isJavaOrUnicodeWhitespace(char)
       */
-    static boolean yetIsGenerallyWhitespace( final char nonSpaceNewline ) {
+    public static boolean yetIsGenerallyWhitespace( final char nonSpaceNewline ) {
         return yetIsGenerallyWhitespace( (int)nonSpaceNewline ); }
 
 
@@ -134,7 +136,7 @@ final class Language {
       *       or newline constituent.
       *     @see Java.Characters.isJavaOrUnicodeWhitespace(int)
       */
-    static boolean yetIsGenerallyWhitespace( final  int nonSpaceNewline ) {
+    public static boolean yetIsGenerallyWhitespace( final  int nonSpaceNewline ) {
         assert !isPlainWhitespace( nonSpaceNewline );
         return Character.isWhitespace/*[TL]*/( nonSpaceNewline ) /* Which test excludes the allowed
             no-break space (A0), plus some forbidden no-break spaces.  Wherefore include the latter: */
@@ -148,4 +150,4 @@ final class Language {
 
 
 
-                                                        // Copyright © 2021  Michael Allan.  Licence MIT.
+                                                   // Copyright © 2021-2022  Michael Allan.  Licence MIT.
